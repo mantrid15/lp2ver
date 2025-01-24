1e0df7ecb268d0d1f306e1e504e5d52812c7ac6c
kozlovskiy_a <doccooper15@gmail.com>
2025-01-24 12:08:02 +0300

    46 commit - продолжение исправления: Gate.vue работает настройка прокрутка внутри разделов

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,53 @@
 <template>
   <v-app>
-    <v-toolbar density="default" dark color="primary" app class="fixed-toolbar">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/linzer">About</RouterLink>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <div class="row-tool-container">
-        <RowTool :buttonColor="buttonColor"
-                 @change-button-color="changeButtonColorHandler"
-                 :userId="userId"></RowTool>
-      </div>
-      <v-btn to="/login"
-             variant="flat"
-             :color="loginButtonColor"
-             class="fixed-size-button"
-
-             style="padding: 5px;"
-             @click="isLoggedIn ? handleLoginStateChange(false) : null"
-
-      ><span style="margin-right: 10px">
+    <header>
+      <v-toolbar density="default" dark color="primary" app class="fixed-toolbar">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/linzer">About</RouterLink>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <div class="row-tool-container">
+          <RowTool :buttonColor="buttonColor"
+                   @change-button-color="changeButtonColorHandler"
+                   :userId="userId"></RowTool>
+        </div>
+        <v-btn to="/login"
+               variant="flat"
+               :color="loginButtonColor"
+               class="fixed-size-button"
+               style="padding: 5px;"
+               @click="isLoggedIn ? handleLoginStateChange(false) : null"
+        ><span style="margin-right: 10px">
         {{ loginButtonText }}
                       </span>
 
-        <v-img
-            src="/lpicon.png"
-            alt="Login Icon"
-            width="20"
-            height="20"
-            class="mr-2 ml-2"
-            :color="loginButtonColor"        />
-      </v-btn>
-    </v-toolbar>
+          <v-img
+              src="/lpicon.png"
+              alt="Login Icon"
+              width="20"
+              height="20"
+              class="mr-2 ml-2"
+              :color="loginButtonColor"        />
+        </v-btn>
+      </v-toolbar>
+    </header>
 
     <v-main class="main-with-margin">
-      <router-view v-slot="{ Component }">
-        <v-fade-transition>
-          <component
-              :is="Component"
-              @changeButtonColor="changeButtonColorHandler"
-              :buttonColor="buttonColor"
-
-          />
-        </v-fade-transition>
-      </router-view>
+      <div class="content">
+        <router-view v-slot="{ Component }">
+          <v-fade-transition>
+            <component
+                :is="Component"
+                @changeButtonColor="changeButtonColorHandler"
+                :buttonColor="buttonColor"
+            />
+          </v-fade-transition>
+        </router-view>
+      </div>
     </v-main>
+
     <v-footer class="custom-footer" dark color="primary" app>
       <v-row justify="center" no-gutters>
         <v-col class="text-center" cols="12">
@@ -62,52 +64,51 @@ import RowTool from "@/components/MainRowTool.vue";
 import { supabase } from "@/clients/supabase";
 import { useStore } from 'vuex';
 
-
 const store = useStore();
-const userId = computed(() => store.state.userId); // Получите userId из Vuex
-const buttonColor = ref('red'); // Начальный цвет кнопки
-const loginButtonText = ref('Login'); // Текст кнопки логина
-// const loginButtonColor = ref('green'); // Цвет кнопки логина
-const loginButtonColor = ref('red'); // Цвет кнопки логина
+const userId = computed(() => store.state.userId);
+const buttonColor = ref('red');
+const loginButtonText = ref('Login');
+const loginButtonColor = ref('red');
 const isLoggedIn = ref(false);
-// Добавляем слушатель состояния аутентификации при монтировании
+
 supabase.auth.onAuthStateChange((event, session) => {
   isLoggedIn.value = !!session;
   handleLoginStateChange(isLoggedIn.value);
 });
 
 const changeButtonColorHandler = (color) => {
-  buttonColor.value = color; // Изменяем цвет кнопки
+  buttonColor.value = color;
 };
+
 const handleLoginStateChange = (isLoggedIn) => {
   if (isLoggedIn) {
     loginButtonText.value = 'Logout';
     loginButtonColor.value = 'purple';
     buttonColor.value = 'purple';
-
   } else {
     loginButtonText.value = 'Login';
     loginButtonColor.value = 'red';
     buttonColor.value = 'red';
   }
 };
-
-
 </script>
 
 <style scoped>
-.main-with-margin {
-  margin-top: 10px; /* Отступ сверху для v-main */
-}
-
-
 .fixed-toolbar {
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
-  margin-bottom: 10px;
-  z-index: 1000;/* !* Убедитесь, что toolbar выше других элементов *!*/
+  z-index: 1000;
+}
+
+.main-with-margin {
+  height: calc(100vh - 85px); /* Высота v-main с учетом header и отступа */
+  overflow: hidden; /* Убираем прокрутку */
+}
+
+.content {
+  padding: 20px; /* Отступ для содержимого */
 }
 .fixed-size-button {
   width: 120px;
@@ -117,13 +118,11 @@ const handleLoginStateChange = (isLoggedIn) => {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-  /*  margin-bottom: 5px;
-    margin-top: 5px;*/
 }
 .row-tool-container {
   display: flex;
   align-items: center;
-  margin-right: 10px; /* Отступ в 50 пикселей от кнопки */
+  margin-right: 10px;
 }
 .red-button {
   background-color: red;
--- src/views/LinzerView.vue
+++ src/views/LinzerView.vue
@@ -1,21 +1,17 @@
 <template>
-<!--  <div class="about" v-bind="$attrs">-->
-<!--    <div class="inforow">
-      <p id="account" v-if="account && account.data && account.data.session">
-        Account: {{ account.data.session.user.email }}
-      </p>
-      <p> Protect the secret password: **TMX$$ </p>
-    </div>-->
-<!--    <div class="row-container">-->
-      <Gate style="margin-top: 50px"></Gate>
-<!--    </div>-->
-<!--  </div>-->
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <Gate class="gate-content"></Gate>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-import { ref, onMounted  } from "vue";
+import { ref, onMounted } from "vue";
 import { supabase } from '@/clients/supabase.js';
-import Gate from  "@/components/Gate.vue";
+import Gate from "@/components/Gate.vue";
 
 const account = ref();
 onMounted(async () => {
@@ -29,62 +25,10 @@ async function getSession() {
 </script>
 
 <style scoped>
-/*.about {
-  margin-top: 60px; !* Отступ сверху *!
-}*/
-
-/*.inforow {
-  height: var(--inforow-height);
-  z-index: 10;
-  display: flex;
-  align-items: center;
-  position: fixed; !* Фиксированное позиционирование *!
-  padding: 10px;
-  background-color: yellow;
-  box-sizing: border-box;
-  width: 100%;
-}*/
-
-/*.inforow p {
-  margin-right: 50px;
-}*/
-
-/*.inforow p:last-child {
-  margin-right: 0;
-}*/
-
-.row-container {
- /* margin-top: var(--inforow-height); !* Отступ равный высоте inforow *!*/
-  padding: 20px;
-  background-color: #f9f9f9;
-  /*
-  min-height: calc(100vh - var(--inforow-height)); !* Высота без высоты inforow *!
-  */
-  overflow: hidden; /* Скрыть вертикальную прокрутку */
-  position: relative; /* Чтобы избежать недопонимания по высоте в родительском элементе */
+.gate-content {
+  top: 39px;
+  max-height: calc(100vh - 50px); /* Высота без учета inforow */
+  overflow: hidden; /* Отключает вертикальную прокрутку */
+  position: relative; /* Сохраняет относительное позиционирование */
 }
-
-/*!* Добавляем стиль для содержимого Gate *!
-.row-container > * {
-  !*max-height: calc(100% - 40px); !* Устанавливаем максимальную высоту для содержимого внутри row-container *!*!
-  overflow: auto; !* Позволяем прокрутку только внутри компонента Gate, если нужно *!
-}
-
-#account {
-  background-color: green;
-}*/
-
-/*
-:root {
-  --inforow-height: 60px; !* Переменная для высоты *!
-}
-*/
-
-
-/*.row-container {
-  margin-top: var(--inforow-height); !* Используем переменную для высоты *!
-  padding: 20px;
-  background-color: #f9f9f9;
-  min-height: calc(100vh - var(--inforow-height));
-}*/
 </style>
