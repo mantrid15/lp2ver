f2b735fc21b5e48520b5019af0810b5cec73dc43
kozlovskiy_a <doccooper15@gmail.com>
2025-02-11 09:17:14 +0300

    95 commit - serever.js добавлена обработка работы запросы с браузером. в кнопку добавлен еще один запрос для запросов черезе браузер. пакетное удаление dir_hash в выбранной папке. окрас yellow-box. при клике на yellow-box возвращение на Limbo.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios'; // Импортируйте axios вместо request
+import puppeteer from 'puppeteer';
 
 const app = express();
 
@@ -19,6 +20,56 @@ app.get('/proxy', async (req, res) => {
         res.send(response.data);
     } catch (error) {
         return res.status(500).json({ error: 'Ошибка при запросе к URL' });
+    }
+});
+
+
+
+// Функция для извлечения метаданных
+async function puppeteerMetaData(url) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    try {
+        console.log(`Открываем страницу: ${url}`);
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+        await page.goto(url, { waitUntil: 'networkidle2' });
+
+        console.log('Страница загружена. Извлекаем мета-данные...');
+
+        // Извлекаем заголовок и мета-данные
+        const metaData = await page.evaluate(() => {
+            const headTitle = document.querySelector('html head title')?.textContent || '';
+            const documentTitle = document.querySelector('title')?.textContent || '';
+            const title = headTitle || documentTitle || '';
+            const description = document.querySelector('meta[name="description"]')?.content || '';
+            const keywords = document.querySelector('meta[name="keywords"]')?.content || '';
+
+            return { title, description, keywords };
+        });
+
+        console.log('Мета-данные извлечены:', metaData);
+        return metaData;
+    } catch (error) {
+        console.error('Ошибка при получении мета-данных:', error.message);
+        return null;
+    } finally {
+        await browser.close();
+    }
+}
+
+// Новый маршрут для извлечения метаданных с помощью Puppeteer
+app.get('/fetch-metadata', async (req, res) => {
+    const url = req.query.url;
+    if (!url) {
+        return res.status(400).json({ error: 'URL не предоставлен' });
+    }
+
+    const metaData = await puppeteerMetaData(url);
+    if (metaData) {
+        res.json(metaData);
+    } else {
+        res.status(500).json({ error: 'Ошибка при получении мета-данных' });
     }
 });
 
