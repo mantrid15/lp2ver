b3f1024f42ea9f20e9974008b92f482c654fe77d
doccooper <doccooper15@gmail.com>
2025-12-05 10:21:44 +0300

    362 - настройка Prod и dev работа с настройкой server.js разделил .env На две ветки, добавлено многое по этим двум направлениям server: исправлены два базовых файла для серверной части. переведен порт 3000 на 3002 в остальных файлах переведен порт 3000 на 3002 так как на сервере на порту 3000 ollama

--- src/clients/supabase.js
+++ src/clients/supabase.js
@@ -1,4 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+// console.log('Supabase URL:', import.meta.env.VITE_SUPABASE_URL)
+// console.log('Supabase Key:', import.meta.env.VITE_SUPABASE_KEY ? 'есть' : 'нет')
+//
+// if (!import.meta.env.VITE_SUPABASE_URL || !import.meta.env.VITE_SUPABASE_KEY) {
+//     console.error('Supabase переменные не загружены!')
+// }
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
